a0a8590b1537c53b214516ae5fc907107fb7e7de
Yuri Lima <yurilimmarabelo@gmail.com>
2023-09-28 21:38:02 +0100

    added Cardapio and Buscador

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import "normalize.css";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Cardapio from './pages/Cardapio';
 
-const root = ReactDOM.createRoot(
+ReactDOM.render(
+<React.StrictMode>
+    <Cardapio />
+  </React.StrictMode>,
   document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
+
+
